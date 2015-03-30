57a6cd8a3a661484bc8077252cc412c31b5c5e32
masdeft <masdeft@gmail.com>
2015-03-30 14:35:22 +0300

    Add routes and Robots model with validation.
    Implement a route to get a list of robots.
    Add model 'Robots' with validation of fields 'type' and 'name'.

---
+++ app/models/Robots.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Phalcon\Mvc\Model;
+use Phalcon\Mvc\Model\Message;
+use Phalcon\Mvc\Model\Validator\Inclusionin;
+use Phalcon\Mvc\Model\Validator\Uniqueness;
+
+class Robots extends Model
+{
+    public function validation()
+    {
+        $this->validate(new Inclusionin(
+            [
+                "field" => "type",
+                "domain" => ["droid", "mechanical", "virtual"]
+            ]
+        ));
+
+        $this->validate(new Uniqueness(
+            [
+                "field" => "name",
+                "message" => "The robot name must be unique"
+            ]
+        ));
+
+        if ($this->year < 0) {
+            $this->appendMessage(new Message("The year cannot be less that zero"));
+        }
+
+        if ($this->validationHasFailed()) {
+            return false;
+        }
+    }
+}
