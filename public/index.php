004b03683b2a12bb57f1b480bbc5c3a36c7bcb06
masdeft <masdeft@gmail.com>
2015-03-30 15:21:11 +0300

    Revert "Revert "Add routes and Robots model with validation.""
    This reverts commit 9f58b5c54a4f68847bf6b88f4648f43c7732dc17.

--- public/index.php
+++ public/index.php
@@ -1,2 +1,57 @@
 <?php
 
+$di = new \Phalcon\DI\FactoryDefault();
+
+$di->set('db', function() {
+    return new \Phalcon\Db\Adapter\Pdo\Mysql([
+        "host" => "localhost",
+        "username" => "rest_api",
+        "password" => "rest_api",
+        "dbname" => "rest_api"
+    ]);
+});
+
+$app = new \Phalcon\Mvc\Micro($di);
+
+// get list of all robots
+$app->get('/api/robots', function() use ($app) {
+    $phql = 'SELECT * FROM robots ORDER BY name';
+    $robots = $app->modelsManager->executeQuery($phql);
+
+    $data = [];
+    foreach ($robots as $robot) {
+        $data[] = [
+            'id' => $robot->id,
+            'name' => $robot->name
+        ];
+    }
+
+    return json_encode($data);
+});
+
+// get a robot by the name
+$app->get('/api/robots/search/{name}', function($name) {
+
+});
+
+// get a robot by the id
+$app->get('/api/robots/id:[0-9]+', function($id) {
+
+});
+
+// add a new robot
+$app->post('/api/robots', function() {
+
+});
+
+// update a robot by the id
+$app->put('/api/robots/id:[0-9]+', function($id) {
+
+});
+
+// delete a robot by the id
+$app->delete('/api/robots/id:[0-9]+', function($id) {
+
+});
+
+$app->handle();
